be028d25d9227c32aad2d5f29ea389b574867af2
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-28 07:40:23 +0300

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to check if it is a palindrome");
+            string text = Console.ReadLine();
+            Console.WriteLine($"The word {text} is a palindrome: {IsPalindrome(text)}");
+        }
+
+        private static bool IsPalindrome(string text)
+        {
+            text = text.ToLower();
+            bool isPalindrom = false;
+
+            for(int i = 0; i < text.Length/2; i++)
+            {
+                if (text[i].Equals(text[text.Length - 1 - i] ))
+                {
+                    isPalindrom = true;
+                }
+                else
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+
+            return isPalindrom;
+        }
+    }
+}
